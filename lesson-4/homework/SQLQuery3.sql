cdaaee0ab733a071a16d0e746eb56bb55b8b1021
lo4in <mehmedusmanli@gmail.com>
2025-03-04 20:35:08 +0500

    dfs

---
+++ lesson-4/homework/SQLQuery3.sql
@@ -0,0 +1,20 @@
+CREATE TABLE [dbo].[TestMultipleZero]
+(
+    [A] [int] NULL,
+    [B] [int] NULL,
+    [C] [int] NULL,
+    [D] [int] NULL
+);
+GO
+
+INSERT INTO [dbo].[TestMultipleZero](A,B,C,D)
+VALUES 
+    (0,0,0,1),
+    (0,0,1,0),
+    (0,1,0,0),
+    (1,0,0,0),
+    (0,0,0,0),
+    (1,1,1,0);
+SELECT * 
+FROM [dbo].[TestMultipleZero]
+WHERE A <> 0 OR B <> 0 OR C <> 0 OR D <> 0;
